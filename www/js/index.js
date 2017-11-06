ff80595606d682f70aafecce5c3448ee41ba1aa0
sanr4-15 <sanr4_15@uni.worc.ac.uk>
2017-11-06 14:51:02 +0000

    initial2

---
+++ www/js/index.js
@@ -0,0 +1,16 @@
+$(document).on("pagecreate","#pageone",function(){
+  $('#submitButton').on("click", function(){
+    submitText();
+  });            
+});            
+
+
+function submitText() {
+	var text = $('#textinput').val();
+	alert(text);
+}
+
+function storeValue(key, value) {
+	//add some code to store the key-value pair in persistant storage 
+    
+}
